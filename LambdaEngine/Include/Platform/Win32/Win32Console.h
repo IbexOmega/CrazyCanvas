0ab31edeecd106707cc6de78ef09cc5378ef7067
Herman <skumbananaaa@gmail.com>
2020-03-26 13:57:49 +0100

    Windows Console

---
+++ LambdaEngine/Include/Platform/Win32/Win32Console.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#ifdef LAMBDA_PLATFORM_WINDOWS
+
+#include "Platform/Common/Console.h"
+
+namespace LambdaEngine
+{
+	class Win32Console : public Console
+	{
+	public:
+		static void Show();
+		static void Close();
+
+		static void Print(const char*, ...);
+		static void PrintLine(const char*, ...);
+
+		static void Clear();
+
+		static void SetColor(EConsoleColor);
+
+	private:
+		static HANDLE s_OutputHandle;
+
+	};
+
+	typedef Win32Console PlatformConsole;
+}
+
+#endif
