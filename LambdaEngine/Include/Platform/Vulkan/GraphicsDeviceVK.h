1b5ae9d6aa0dbe6e2bf028c9168fe719f6d286eb
Herman <skumbananaaa@gmail.com>
2020-03-30 19:22:11 +0200

    Added Vulkan to Premake + Vulkan Instance Stuff Started

--- LambdaEngine/Include/Platform/Vulkan/GraphicsDeviceVK.h
+++ LambdaEngine/Include/Platform/Vulkan/GraphicsDeviceVK.h
@@ -1,6 +1,59 @@
-#include "Rendering/Core/IGraphicsContext.h"
+#pragma once
+#include "Rendering/Core/IGraphicsDevice.h"
+#include "Vulkan.h"
+
+#include <vector>
 
 namespace LambdaEngine
 {
-    
+	class GraphicsDeviceVK : public IGraphicsDevice
+	{
+	public:
+		GraphicsDeviceVK(); 
+		~GraphicsDeviceVK();
+
+		virtual bool Init(const GraphicsDeviceDesc& desc) override;
+		virtual void Release() override;
+
+		//CREATE
+		virtual IRenderPass*		CreateRenderPass() override;
+		virtual IFence*				CreateFence() override;
+		virtual ICommandList*		CreateCommandList() override;
+		virtual IPipelineState*		CreatePipelineState() override;
+		virtual IBuffer*			CreateBuffer() override;
+		virtual ITexture*			CreateTexture() override;
+		virtual ITextureView*		CreateTextureView() override;
+		
+	private:
+		//INIT
+		bool InitInstance(const GraphicsDeviceDesc& desc);
+
+		bool InitDevice();
+		bool InitPhysicalDevice();
+		bool InitLogicalDevice();
+
+		//UTIL
+		bool ValidationLayersSupported();
+
+		int32 RatePhysicalDevice(VkPhysicalDevice physicalDevice);
+
+	private:
+		static VKAPI_ATTR VkBool32 VKAPI_CALL DebugCallback(
+			VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+			VkDebugUtilsMessageTypeFlagsEXT messageType,
+			const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+			void* pUserData);
+
+	public:
+		VkInstance Instance;
+
+		VkPhysicalDevice PhysicalDevice;
+		VkDevice Device;
+
+	private:
+		std::vector<const char*> m_ValidationLayers;
+
+		std::vector<const char*> m_RequiredInstanceExtensions;
+		std::vector<const char*> m_OptionalInstanceExtensions;
+	};
 }
---
+++ LambdaEngine/Include/Rendering/Core/IGraphicsDevice.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "LambdaEngine.h"
+
+namespace LambdaEngine
+{
+	class IRenderPass;
+	class IFence;
+	class ICommandList;
+	class IPipelineState;
+	class IBuffer;
+	class ITexture;
+	class ITextureView;
+
+	struct GraphicsDeviceDesc
+	{
+		bool Debug;
+	};
+
+	class IGraphicsDevice
+	{
+	public:
+		DECL_INTERFACE(IGraphicsDevice);
+
+		virtual bool Init(const GraphicsDeviceDesc& desc) = 0;
+		virtual void Release() = 0;
+
+		virtual IRenderPass*		CreateRenderPass() = 0;
+		virtual IFence*				CreateFence() = 0;
+		virtual ICommandList*		CreateCommandList() = 0;
+		virtual IPipelineState*		CreatePipelineState() = 0;
+		virtual IBuffer*			CreateBuffer() = 0;
+		virtual ITexture*			CreateTexture() = 0;
+		virtual ITextureView*		CreateTextureView() = 0;
+	};
+}
