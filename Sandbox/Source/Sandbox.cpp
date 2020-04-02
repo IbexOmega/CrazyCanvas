57cffa43e4c999fae689c3966f4e986cacb0f167
Herman <skumbananaaa@gmail.com>
2020-04-02 23:45:46 +0200

    Fixed premake, FMOD must be on D

--- Sandbox/Source/Sandbox.cpp
+++ Sandbox/Source/Sandbox.cpp
@@ -15,74 +15,7 @@ Sandbox::Sandbox() :
 	m_pResourceManager(nullptr)
 {
 	using namespace LambdaEngine;
-    
-	//TCP TEST
-	/*ISocketTCP* server = PlatformSocketFactory::CreateSocketTCP();
-	server->Bind("127.0.0.1", 4444);
-	server->Listen();
 
-	ISocketTCP* client = PlatformSocketFactory::CreateSocketTCP();
-	client->Connect("127.0.0.1", 4444);
-
-	ISocketTCP* serverClient = server->Accept();
-
-	std::string data = "Hello Guy!";
-	uint32 bytesSent;
-	serverClient->Send(data.c_str(), data.length(), bytesSent);
-
-	char buffer[256];
-	uint32 bytesReceived;
-	client->Receive(buffer, 256, bytesReceived);
-
-	LOG_MESSAGE(buffer);
-	serverClient->Close();
-	client->Close();
-	server->Close();
-
-
-	//UDP TEST
-	ISocketUDP* socket1 = PlatformSocketFactory::CreateSocketUDP();
-	ISocketUDP* socket2 = PlatformSocketFactory::CreateSocketUDP();
-
-	socket2->Bind("127.0.0.1", 4444);
-	socket1->SendTo(data.c_str(), data.length(), bytesSent, "127.0.0.1", 4444);
-
-	buffer[256];
-	std::string sender;
-	uint16 port;
-	socket2->ReceiveFrom(buffer, 256, bytesReceived, sender, port);
-	LOG_MESSAGE(buffer);
-	LOG_MESSAGE(sender.c_str());
-	LOG_MESSAGE("%d", port);
-
-	data = "Vafan Guy!";
-	socket2->SendTo(data.c_str(), data.length(), bytesSent, sender, port);
-	socket1->ReceiveFrom(buffer, 256, bytesReceived, sender, port);
-	LOG_MESSAGE(buffer);
-	LOG_MESSAGE(sender.c_str());
-	LOG_MESSAGE("%d", port);
-	socket1->Close();
-	socket2->Close();
-
-
-	//UDP Broadcast TEST
-	LOG_MESSAGE("Broadcast");
-	socket1 = PlatformSocketFactory::CreateSocketUDP();
-	socket2 = PlatformSocketFactory::CreateSocketUDP();
-
-	socket1->EnableBroadcast();
-	socket2->EnableBroadcast();
-
-	socket1->Bind("", 4444);
-
-	data = "Ny data Guy!";
-
-	socket2->Broadcast(data.c_str(), data.length(), bytesSent, 4444);
-
-	socket1->ReceiveFrom(buffer, 256, bytesReceived, sender, port);
-	LOG_MESSAGE(buffer);
-	LOG_MESSAGE(sender.c_str());
-	LOG_MESSAGE("%d", port);*/
 
 	m_pResourceManager = new LambdaEngine::ResourceManager(LambdaEngine::RenderSystem::GetDevice(), LambdaEngine::AudioSystem::GetDevice());
 	m_TestSound = m_pResourceManager->LoadSoundFromFile("../Assets/Sounds/smb_gameover.wav", ESoundFlags::LOOPING);
