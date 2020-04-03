27b868ba4949796f785ece0ed6dec3abbe8242d7
Alexander Dahlin <alexander@dahlins.nu>
2020-04-04 00:19:18 +0200

    Added documentation

--- LambdaEngine/Include/Application/API/Misc.h
+++ LambdaEngine/Include/Application/API/Misc.h
@@ -11,8 +11,20 @@ namespace LambdaEngine
 	public:
 		DECL_STATIC_CLASS(Misc);
 
+		/*
+		* Displays a message window, this is a blocking call
+		*
+		* pCaption 	- Title of the window
+		* pText		- Text to display
+		*/
 		static void MessageBox(const char*, const char*)		{ }
 
+		/*
+		* Outputs a message to the debugger
+		* 
+		* pFormat 	- A formated string to display
+		* args		- Arguments for the string
+		*/
         static void OutputDebugString(const char*, ...)			{ }
         static void OutputDebugStringV(const char*, va_list)	{ }
 	};
--- LambdaEngine/Include/Application/API/Window.h
+++ LambdaEngine/Include/Application/API/Window.h
@@ -14,7 +14,22 @@ namespace LambdaEngine
         
         virtual void SetTitle(const char* pTitle) = 0;
         
+        /*
+        * Retrive the native handle
+        * 
+        * return -  Return the native handle of the window. 
+        *           Win32:  HWND
+        *           Mac:    NSWindow*
+        */
         virtual void*       GetHandle() const = 0;
+
+        /*
+        * Return a handle to the client area of the window. May be nullptr on some platforms
+        * 
+        * return -  Returns a handle to the client-area of the window
+        *           Win32:  nullptr
+        *           Mac:    NSView*
+        */
         virtual const void* GetView()   const = 0;
     };
 }
