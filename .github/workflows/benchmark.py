bc084e594041a3be0ad5790a270a13e89049cbcc
TheoBerlin <theo.holmqvist@gmail.com>
2020-09-07 17:40:33 +0200

    Add workflow configs and scripts

---
+++ .github/workflows/benchmark.py
@@ -0,0 +1,81 @@
+import json, os, subprocess, sys, getopt
+
+BENCHMARK_RESULTS_PATH          = 'benchmark_results.json'
+BENCHMARK_RESULTS_PATH_RT_ON    = 'benchmark_results_rt_on.json'
+BENCHMARK_RESULTS_PATH_RT_OFF   = 'benchmark_results_rt_off.json'
+
+ENGINE_CONFIG_PATH      = '../engine_config.json'
+TEMP_ENGINE_CONFIG_PATH = '../engine_config_copy.json'
+
+def print_help(help_string, args):
+	print('Intended usage:')
+	print(help_string)
+	print(f'Used flags: {str(args)}')
+
+def remove_existing_benchmark_files():
+	for file_name in [BENCHMARK_RESULTS_PATH, BENCHMARK_RESULTS_PATH_RT_ON, BENCHMARK_RESULTS_PATH_RT_OFF]:
+		if os.path.exists(file_name):
+			os.remove(file_name)
+
+def set_engine_config(ray_tracing_enabled):
+	with open(ENGINE_CONFIG_PATH, 'w+') as cfgFile:
+		config = {}
+		config['RayTracingEnabled'] = ray_tracing_enabled
+		cfgFile.seek(0)
+		json.dump(config, cfgFile, indent=4)
+		cfgFile.truncate()
+		cfgFile.close()
+
+def run_benchmark(bin_path, ray_tracing_enabled):
+	set_engine_config(ray_tracing_enabled)
+	print('Benchmarking with ray tracing {}... '.format('enabled' if ray_tracing_enabled else 'disabled'), end='', flush=True)
+	completed_process = subprocess.run([bin_path], capture_output=True)
+	if completed_process.returncode != 0:
+		print(f'Failed:\n{completed_process.stdout}\n\n{completed_process.stderr}')
+		sys.exit(1)
+
+	print(' Success')
+
+def main(argv):
+	help_str = '''usage: --bin <binpath>\n
+		bin: path to application binary'''
+	try:
+		opts, args = getopt.getopt(argv, 'h', ['help', 'bin='])
+	except getopt.GetoptError:
+		print_help(help_str, args)
+		sys.exit(1)
+
+	bin_path = None
+	for opt, arg in opts:
+		if opt in ['-h', '--help']:
+			print_help(help_str, args)
+			sys.exit(1)
+		if opt == '--bin':
+			bin_path = arg
+
+	if not bin_path:
+		print('Missing argument')
+		print_help(help_str, args)
+		sys.exit(1)
+
+	os.chdir('CrazyCanvas')
+	bin_path = '../' + bin_path
+
+	remove_existing_benchmark_files()
+
+	# Temporarily rename engine config file
+	if not os.path.exists(TEMP_ENGINE_CONFIG_PATH):
+		os.rename(ENGINE_CONFIG_PATH, TEMP_ENGINE_CONFIG_PATH)
+
+	run_benchmark(bin_path, ray_tracing_enabled=True)
+	os.rename(BENCHMARK_RESULTS_PATH, BENCHMARK_RESULTS_PATH_RT_ON)
+
+	run_benchmark(bin_path, ray_tracing_enabled=False)
+	os.rename(BENCHMARK_RESULTS_PATH, BENCHMARK_RESULTS_PATH_RT_OFF)
+
+	# Restore original engine config
+	os.remove(ENGINE_CONFIG_PATH)
+	os.rename(TEMP_ENGINE_CONFIG_PATH, ENGINE_CONFIG_PATH)
+
+if __name__ == '__main__':
+	main(sys.argv[1:])
---
+++ .github/workflows/read-lint-report.py
@@ -0,0 +1,104 @@
+import json, re, sys, getopt
+
+# Regexes that will catch messages to be suppressed
+SUPPRESSED_REGEXES = [
+    "Class '.*' has a constructor with 1 argument that is not explicit",
+    "Consider using std::transform",
+    "The function '.*' is never used",
+    "Class '.*' does not have a .* which is recommended since it has dynamic memory",
+    r"^\(information\)"
+]
+
+def isSuppressed(line):
+    for suppressedRegex in SUPPRESSED_REGEXES:
+        if re.search(suppressedRegex, line) is not None:
+            return True
+    return False
+
+# Returns true if a warning was printed
+def printWarning(line, filesToLint):
+    fileRegex       = r"\[([^:]+):([0-9]+)\]"
+    anything        = ".*"
+    categoryRegex   = r"(\([^)]+\))"
+    messageRegex    = "(.*?)$"
+    match           = re.search(rf"{fileRegex}(?:{anything}{fileRegex})?{anything}{categoryRegex}\s{messageRegex}", line)
+    if match is None or len(match.groups()) != 6:
+        print(f"Failed to regex search string: {line}")
+        return False
+
+    searchResults   = match.groups()
+
+    firstFileName   = searchResults[0]
+    firstLineNr     = searchResults[1]
+    lintCategory    = searchResults[4]
+    message         = searchResults[5]
+    secondFileName  = ""
+    secondLineNr    = ""
+
+    outMessage = ""
+    if searchResults[2] is not None and searchResults[3] is not None:
+        # The line contains two files. Have the second one be printed in the output message
+        secondFileName  = searchResults[2]
+        secondLineNr    = searchResults[3]
+        outMessage += f"[{secondFileName}:{secondLineNr}]: "
+
+    if firstFileName not in filesToLint and secondFileName not in filesToLint:
+        return False
+
+    outMessage += f"{lintCategory} {message}"
+    print(f"::warning file={firstFileName},line={firstLineNr}::{outMessage}")
+    return True
+
+def readReport(fileName, filesToLint):
+    suppressedMessages = []
+    warningCount = 0
+
+    with open(fileName) as file:
+        for line in file:
+            if isSuppressed(line):
+                suppressedMessages.append(line)
+            elif printWarning(line, filesToLint):
+                warningCount += 1
+
+    print(f"Warnings: {warningCount}, suppressed messages: {len(suppressedMessages)}")
+    print("Suppressed messages:")
+    for suppressedMessage in suppressedMessages:
+        print(suppressedMessage, end='')
+
+def getFilesToLint(modifiedFilesPath, addedFilesPath):
+    modifiedFiles = json.load(open(modifiedFilesPath, 'r')) if modifiedFilesPath != "" else []
+    addedFiles = json.load(open(addedFilesPath, 'r')) if addedFilesPath != "" else []
+    print(modifiedFiles)
+    print(addedFiles)
+    return modifiedFiles + addedFiles
+
+def main(argv):
+    inputFile = ""
+    modifiedFilesPath = ""
+    addedFilesPath = ""
+    helpStr = '''usage: read-lint-report.py --report <path> --modified-files <path> --added-files <path>\n
+        modified-files: path to csv file containing file paths of files modified in a merge request\n
+        added-files: path to csv file containing file paths of files added in a merge request'''
+    try:
+        opts, args = getopt.getopt(argv, "h", ["report=", "modified-files=", "added-files="])
+    except getopt.GetoptError:
+        print("Intended usage:")
+        print(helpStr)
+        print(f"Used flags: {str(args)}")
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == "-h":
+            print(helpStr)
+            sys.exit(1)
+        elif opt == "--report":
+            inputFile = arg
+        elif opt == "--modified-files":
+            modifiedFilesPath = arg
+        elif opt == "--added-files":
+            addedFilesPath = arg
+
+    filesToLint = getFilesToLint(modifiedFilesPath, addedFilesPath)
+    readReport(inputFile, filesToLint)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
