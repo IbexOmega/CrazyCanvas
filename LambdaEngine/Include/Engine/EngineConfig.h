b6ca702c4b67b702db596841fc14738590dbb449
Vonsen <vonsenmc@gmail.com>
2020-09-11 00:34:58 +0200

    added setters and getters
    set array needs to be fixed

--- LambdaEngine/Include/Engine/EngineConfig.h
+++ LambdaEngine/Include/Engine/EngineConfig.h
@@ -13,8 +13,17 @@ namespace LambdaEngine
         DECL_STATIC_CLASS(EngineConfig);
 
         static bool LoadFromFile();
+        static bool WriteToFile();
 
         static bool GetBoolProperty(const String& propertyName);
+        static float GetFloatProperty(const String& propertyName);
+        static int GetIntProperty(const String& propertyName);
+        static TArray<int> GetArrayProperty(const String& propertyName);
+
+        static bool SetBoolProperty(const String& propertyName, bool value);
+        static bool SetFloatProperty(const String& propertyName, float value);
+        static bool SetIntProperty(const String& propertyName, int value);
+        static bool SetArrProperty(const String& propertName, int* arr);
 
     private:
         static rapidjson::Document s_ConfigDocument;
--- LambdaEngine/Source/Engine/EngineConfig.cpp
+++ LambdaEngine/Source/Engine/EngineConfig.cpp
@@ -3,18 +3,20 @@
 #include "Log/Log.h"
 
 #include <rapidjson/filereadstream.h>
+#include <rapidjson/filewritestream.h>
+#include <rapidjson/writer.h>
 
 #include <fstream>
 
 namespace LambdaEngine
 {
     rapidjson::Document EngineConfig::s_ConfigDocument = {};
+    using namespace rapidjson;
 
     bool EngineConfig::LoadFromFile()
     {
         const char* pEngineConfigPath = "../engine_config.json";
 
-        using namespace rapidjson;
         FILE* pFile = fopen(pEngineConfigPath, "r");
         if (!pFile)
         {
@@ -32,8 +34,72 @@ namespace LambdaEngine
         return true;
     }
 
+    bool EngineConfig::WriteToFile()
+    {
+        const char* pEngineConfigPath = "../engine_config.json";
+
+        FILE* pFile = fopen(pEngineConfigPath, "wb");
+        if (!pFile)
+        {
+            LOG_WARNING("Engine config could not be opened: %s", pEngineConfigPath);
+            return false;
+        }
+
+        char writeBuffer[2048];
+        FileWriteStream outputStream(pFile, writeBuffer, sizeof(writeBuffer));
+
+        Writer<FileWriteStream> writer(outputStream);
+        s_ConfigDocument.Accept(writer);
+
+        return true;
+    }
+
     bool EngineConfig::GetBoolProperty(const String& propertyName)
     {
         return s_ConfigDocument[propertyName.c_str()].GetBool();
     }
+    float EngineConfig::GetFloatProperty(const String& propertyName)
+    {
+        return s_ConfigDocument[propertyName.c_str()].GetFloat();
+    }
+    int EngineConfig::GetIntProperty(const String& propertyName)
+    {
+        return s_ConfigDocument[propertyName.c_str()].GetInt();
+    }
+
+    TArray<int> EngineConfig::GetArrayProperty(const String& propertyName)
+    {
+        const Value& arr = s_ConfigDocument[propertyName.c_str()];
+        TArray<int> tArr;
+        for (auto& itr : arr.GetArray())
+            tArr.PushBack(itr.GetInt());
+        return tArr;
+    }
+
+    bool EngineConfig::SetBoolProperty(const String& propertyName, bool value)
+    {
+        s_ConfigDocument[propertyName.c_str()].SetBool(value);
+        return true;
+    }
+
+    bool EngineConfig::SetFloatProperty(const String& propertyName, float value)
+    {
+        s_ConfigDocument[propertyName.c_str()].SetFloat(value);
+        return true;
+    }
+
+    bool EngineConfig::SetIntProperty(const String& propertyName, int value)
+    {
+        s_ConfigDocument[propertyName.c_str()].SetFloat(value);
+        return true;
+    }
+
+    bool EngineConfig::SetArrProperty(const String& propertName, int* arr)
+    {
+        
+        s_ConfigDocument[propertName.c_str()].SetArray(arr);
+        return true;
+    }
+
+
 }
