2db409f76608cc9a539935f98f04b2812a5a6eb5
ADRIAN NORDIN <adno16@student.bth.se>
2020-09-10 13:18:58 +0200

    Changed rendegraph to cubemap Test

--- LambdaEngine/Source/Rendering/Renderer.cpp
+++ LambdaEngine/Source/Rendering/Renderer.cpp
@@ -58,7 +58,7 @@ namespace LambdaEngine
 		{
 			RenderGraphStructureDesc renderGraphStructure = {};
 
-			if (!RenderGraphSerializer::LoadAndParse(&renderGraphStructure, "DEMO.lrg", IMGUI_ENABLED))
+			if (!RenderGraphSerializer::LoadAndParse(&renderGraphStructure, "CUBEMAPTEST.lrg", IMGUI_ENABLED))
 			{
 				return false;
 			}
