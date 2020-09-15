8c835e4cea0c3b45fea06c5870855d0928a2d18c
TheoBerlin <theo.holmqvist@gmail.com>
2020-09-15 13:02:41 +0200

    Stop storing ECSCore pointers

--- LambdaEngine/Source/ECS/EntitySubscriber.cpp
+++ LambdaEngine/Source/ECS/EntitySubscriber.cpp
@@ -12,7 +12,7 @@ namespace LambdaEngine
         // Add the component accesses in the component groups to the component accesses vector
         for (const IComponentGroup* pComponentGroup : componentGroups)
         {
-            const TArray<ComponentAccess> groupAccesses = pComponentGroup->ToVector();
+            const TArray<ComponentAccess> groupAccesses = pComponentGroup->ToArray();
             componentAccesses.Insert(componentAccesses.end(), groupAccesses.begin(), groupAccesses.end());
         }
 
@@ -27,18 +27,13 @@ namespace LambdaEngine
         :EntitySubscriptionRegistration({}, componentGroups, pSubscriber, onEntityAdded, onEntityRemoved)
     {}
 
-    EntitySubscriber::EntitySubscriber(ECSCore* pECS)
-        :m_pECS(pECS),
-        m_SubscriptionID(UINT32_MAX)
-    {}
-
     EntitySubscriber::~EntitySubscriber()
     {
-        m_pECS->GetEntityPublisher()->UnsubscribeFromComponents(m_SubscriptionID);
+        ECSCore::GetInstance()->GetEntityPublisher()->UnsubscribeFromComponents(m_SubscriptionID);
     }
 
     void EntitySubscriber::SubscribeToEntities(const EntitySubscriberRegistration& subscriberRegistration, const std::function<bool()>& initFn)
     {
-        m_pECS->EnqueueEntitySubscriptions(subscriberRegistration, initFn, &m_SubscriptionID);
+        ECSCore::GetInstance()->EnqueueEntitySubscriptions(subscriberRegistration, initFn, &m_SubscriptionID);
     }
 }
