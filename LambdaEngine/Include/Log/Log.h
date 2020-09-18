8779279028e03fba6bea9f9c38f9524dfbd6c995
Alexander Dahlin <alexander.dahlins@gmail.com>
2020-09-18 18:31:44 +0200

    Console window does no longer spam same message after each other

--- LambdaEngine/Include/Log/Log.h
+++ LambdaEngine/Include/Log/Log.h
@@ -65,7 +65,7 @@ namespace LambdaEngine
 		* args      - Arguments for the formatted string
 		*/
 		static void Print(ELogSeverity severity, const char* pFormat, ...);
-		static void PrintV(ELogSeverity severity, const char* pFormat, va_list args);
+		static void PrintV(ELogSeverity severity, const char* pFormat, va_list vaArgs);
 
 		/*
 		* Prints an error message with the function-signature where the error occured
@@ -75,7 +75,7 @@ namespace LambdaEngine
 		* args      - The arguments to the formatted string
 		*/
 		static void PrintTraceError(const char* pFunction, const char* pFormat, ...);
-		static void PrintTraceErrorV(const char* pFunction, const char* pFormat, va_list args);
+		static void PrintTraceErrorV(const char* pFunction, const char* pFormat, va_list vaArgs);
 
 		/*
 		* Enables the log to print to the debugger using PlatformMisc::OutputDebugString
