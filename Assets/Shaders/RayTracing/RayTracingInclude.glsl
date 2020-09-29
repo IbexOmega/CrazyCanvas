ae9c9cc1ef4fde83ba80aefe333d5b065355ad16
Vonsen <vonsenmc@gmail.com>
2020-09-29 10:24:12 +0200

    raytracing works

--- Assets/Shaders/RayTracing/RayTracingInclude.glsl
+++ Assets/Shaders/RayTracing/RayTracingInclude.glsl
@@ -24,16 +24,14 @@ layout(binding = 2,     set = BUFFER_SET_INDEX) uniform MaterialParameters  { SM
 
 layout(binding = 0,     set = TEXTURE_SET_INDEX) uniform sampler2D                  u_AlbedoMaps[MAX_UNIQUE_MATERIALS];
 layout(binding = 1,     set = TEXTURE_SET_INDEX) uniform sampler2D                  u_NormalMaps[MAX_UNIQUE_MATERIALS];
-layout(binding = 2,     set = TEXTURE_SET_INDEX) uniform sampler2D                  u_AOMaps[MAX_UNIQUE_MATERIALS];
-layout(binding = 3,     set = TEXTURE_SET_INDEX) uniform sampler2D                  u_RoughnessMaps[MAX_UNIQUE_MATERIALS];
-layout(binding = 4,     set = TEXTURE_SET_INDEX) uniform sampler2D                  u_MetallicMaps[MAX_UNIQUE_MATERIALS];
-layout(binding = 5,     set = TEXTURE_SET_INDEX) uniform samplerCube                u_Skybox;
+layout(binding = 2,     set = TEXTURE_SET_INDEX) uniform sampler2D                  u_CombinedMaterialMaps[MAX_UNIQUE_MATERIALS];
+layout(binding = 3,     set = TEXTURE_SET_INDEX) uniform samplerCube                u_Skybox;
 
-layout(binding = 6,     set = TEXTURE_SET_INDEX, rgba16f) restrict uniform image2D  u_GBufferPosition;
-layout(binding = 7,     set = TEXTURE_SET_INDEX, rgba8) restrict uniform image2D    u_GBufferAlbedo;
-layout(binding = 8,     set = TEXTURE_SET_INDEX, rgba8) restrict uniform image2D    u_GBufferAORoughMetalValid;
-layout(binding = 9,     set = TEXTURE_SET_INDEX, rg16f) restrict uniform image2D    u_GBufferCompactNormal;
-layout(binding = 10,    set = TEXTURE_SET_INDEX) uniform sampler2D 	                u_GBufferDepthStencil;
+layout(binding = 4,     set = TEXTURE_SET_INDEX, rgba16f) restrict uniform image2D  u_GBufferPosition;
+layout(binding = 5,     set = TEXTURE_SET_INDEX, rgba8) restrict uniform image2D    u_GBufferAlbedo;
+layout(binding = 6,     set = TEXTURE_SET_INDEX, rgba8) restrict uniform image2D    u_GBufferAORoughMetalValid;
+layout(binding = 7,     set = TEXTURE_SET_INDEX, rg16f) restrict uniform image2D    u_GBufferCompactNormal;
+layout(binding = 8,    set = TEXTURE_SET_INDEX) uniform sampler2D 	                u_GBufferDepthStencil;
 
 SRayDirections CalculateRayDirections(vec3 hitPosition, vec3 normal, vec3 cameraPosition)
 {
