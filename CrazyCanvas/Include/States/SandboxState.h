86afa0de398b331156fce7b70d789772574e7385
Henry Bergström <henrybergstrom@protonmail.com>
2020-10-06 11:13:10 +0200

    Added SanboxState to CrazyCanvas

---
+++ CrazyCanvas/Include/States/SandboxState.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "Game/State.h"
+#include "ECS/Entity.h"
+
+#include "ECS/ECSCore.h"
+#include "Containers/TArray.h"
+
+#include "Application/API/Events/KeyEvents.h"
+
+#include "NsCore/Ptr.h"
+#include "NsGui/IView.h"
+
+namespace LambdaEngine
+{
+	class StateManager;
+	class ECSCore;
+}
+
+class GUITest;
+
+class SandboxState : public LambdaEngine::State
+{
+public:
+	SandboxState();
+	SandboxState(LambdaEngine::State* pOther);
+	~SandboxState();
+
+	void Init() override final;
+
+	void Resume() override final;
+	void Pause() override final;
+
+	void Tick(LambdaEngine::Timestamp delta);
+
+private:
+	bool OnKeyPressed(const LambdaEngine::KeyPressedEvent& event);
+
+private:
+	LambdaEngine::Entity m_DirLight;
+	LambdaEngine::Entity m_PointLights[3];
+	LambdaEngine::TArray<LambdaEngine::Entity> m_Entities;
+
+	Noesis::Ptr<GUITest> m_GUITest;
+	Noesis::Ptr<Noesis::IView> m_View;
+};
--- CrazyCanvas/Source/GUI/MainMenuGUI.cpp
+++ CrazyCanvas/Source/GUI/MainMenuGUI.cpp
@@ -1,16 +1,19 @@
+#include "Application/API/CommonApplication.h"
+#include "Audio/AudioAPI.h"
+#include "Engine/EngineConfig.h"
 #include "GUI/MainMenuGUI.h"
-
 #include "GUI/Core/GUIApplication.h"
 #include "NoesisPCH.h"
 
+#include "Game/ECS/Systems/Rendering/RenderSystem.h"
 #include "Game/StateManager.h"
 #include "Game/State.h"
+#include "Input/API/InputActionSystem.h"
 #include "States/BenchmarkState.h"
 #include "States/PlaySessionState.h"
+#include "States/SandboxState.h"
 
-#include "Game/ECS/Systems/Rendering/RenderSystem.h"
-#include "Engine/EngineConfig.h"
-#include "Audio/AudioAPI.h"
+
 
 using namespace LambdaEngine;
 using namespace Noesis;
@@ -19,9 +22,14 @@ MainMenuGUI::MainMenuGUI(const LambdaEngine::String& xamlFile)
 {
 	GUI::LoadComponent(this, xamlFile.c_str());
 
-
+	m_pMainMenu = FrameworkElement::FindName<GroupBox>("MainMenuGroupBox");
+	NS_ASSERT(m_pKeybindingsGroupBox != 0);
+	m_pStatesGroupBox = FrameworkElement::FindName<GroupBox>("StatesGroupBox");
+	NS_ASSERT(m_pKeybindingsGroupBox != 0);
 	m_pSettingsGroupBox = FrameworkElement::FindName<GroupBox>("SettingsGroupBox");
 	NS_ASSERT(m_pSettingsGroupBox != 0);
+	m_pKeybindingsGroupBox = FrameworkElement::FindName<GroupBox>("KeybindingsGroupBox");
+	NS_ASSERT(m_pKeybindingsGroupBox != 0);
 	m_pVolumeSlider = FrameworkElement::FindName<Slider>("VolumeSlider");
 	NS_ASSERT(m_pVolumeSlider != 0);
 
@@ -30,6 +38,14 @@ MainMenuGUI::MainMenuGUI(const LambdaEngine::String& xamlFile)
 	m_pVolumeSlider->SetValue(volume);
 	AudioAPI::GetDevice()->SetMasterVolume(volume);
 
+	// Init Keybindings
+	FrameworkElement::FindName<TextBox>("CAM_FORWARD")->SetText(KeyToString(InputActionSystem::GetKey("CAM_FORWARD")));
+	FrameworkElement::FindName<TextBox>("CAM_BACKWARD")->SetText(KeyToString(InputActionSystem::GetKey("CAM_BACKWARD")));
+	FrameworkElement::FindName<TextBox>("CAM_LEFT")	->SetText(KeyToString(InputActionSystem::GetKey("CAM_LEFT")));
+	FrameworkElement::FindName<TextBox>("CAM_RIGHT")->SetText(KeyToString(InputActionSystem::GetKey("CAM_RIGHT")));
+	FrameworkElement::FindName<TextBox>("CAM_JUMP")	->SetText(KeyToString(InputActionSystem::GetKey("CAM_JUMP")));
+	FrameworkElement::FindName<TextBox>("CAM_SPEED_MODIFIER")->SetText(KeyToString(InputActionSystem::GetKey("CAM_SPEED_MODIFIER")));
+	
 	m_RayTracingEnabled = EngineConfig::GetBoolProperty("RayTracingEnabled");
 }
 
@@ -42,9 +58,15 @@ bool MainMenuGUI::ConnectEvent(BaseComponent* pSource, const char* pEvent, const
 	NS_CONNECT_EVENT_DEF(pSource, pEvent, pHandler);
 
 	NS_CONNECT_EVENT(Button, Click, OnButtonSingleplayerClick);
+	NS_CONNECT_EVENT(Button, Click, OnButtonCrazyCanvasClick);
+	NS_CONNECT_EVENT(Button, Click, OnButtonSandboxClick);
+	NS_CONNECT_EVENT(Button, Click, OnButtonBenchmarkClick);
 	NS_CONNECT_EVENT(Button, Click, OnButtonMultiplayerClick);
 	NS_CONNECT_EVENT(Button, Click, OnButtonSettingsClick);
+	NS_CONNECT_EVENT(Button, Click, OnButtonKeybindingsClick);
 	NS_CONNECT_EVENT(Button, Click, OnButtonBackClick);
+	NS_CONNECT_EVENT(Button, Click, OnButtonExitClick);
+	NS_CONNECT_EVENT(Button, Click, OnButtonApplyClick);
 	NS_CONNECT_EVENT(CheckBox, Click, OnRayTracingChecked);
 	NS_CONNECT_EVENT(CheckBox, Click, OnMeshShadersChecked);
 	NS_CONNECT_EVENT(Slider, ValueChanged, OnVolumeSliderChanged);
@@ -57,11 +79,41 @@ void MainMenuGUI::OnButtonSingleplayerClick(BaseComponent* pSender, const Routed
 	UNREFERENCED_VARIABLE(pSender);
 	UNREFERENCED_VARIABLE(args);
 
-	using namespace LambdaEngine;
+	m_ContextStack.push(m_pStatesGroupBox);
+	ToggleView(m_pStatesGroupBox);
+	m_pMainMenu->SetVisibility(Visibility::Visibility_Collapsed);
+}
+
+
+void MainMenuGUI::OnButtonCrazyCanvasClick(BaseComponent* pSender, const RoutedEventArgs& args)
+{
+	UNREFERENCED_VARIABLE(pSender);
+	UNREFERENCED_VARIABLE(args);
+
 	SetRenderStagesSleeping();
 
 	State* pStartingState = DBG_NEW PlaySessionState();
 	StateManager::GetInstance()->EnqueueStateTransition(pStartingState, STATE_TRANSITION::POP_AND_PUSH);
+}
+
+void MainMenuGUI::OnButtonSandboxClick(BaseComponent* pSender, const RoutedEventArgs& args)
+{
+	UNREFERENCED_VARIABLE(pSender);
+	UNREFERENCED_VARIABLE(args);
+
+	SetRenderStagesSleeping();
+	State* pSandboxState = DBG_NEW SandboxState();
+	StateManager::GetInstance()->EnqueueStateTransition(pSandboxState, STATE_TRANSITION::POP_AND_PUSH);
+}
+
+void MainMenuGUI::OnButtonBenchmarkClick(BaseComponent* pSender, const RoutedEventArgs& args)
+{
+	UNREFERENCED_VARIABLE(pSender);
+	UNREFERENCED_VARIABLE(args);
+
+	SetRenderStagesSleeping();
+	State* pBenchmarkState = DBG_NEW BenchmarkState();
+	StateManager::GetInstance()->EnqueueStateTransition(pBenchmarkState, STATE_TRANSITION::POP_AND_PUSH);
 }
 
 void MainMenuGUI::OnButtonMultiplayerClick(BaseComponent* pSender, const RoutedEventArgs& args)
@@ -69,8 +121,7 @@ void MainMenuGUI::OnButtonMultiplayerClick(BaseComponent* pSender, const RoutedE
 	UNREFERENCED_VARIABLE(pSender);
 	UNREFERENCED_VARIABLE(args);
 
-	using namespace LambdaEngine;
-	SetRenderStagesSleeping();
+	//SetRenderStagesSleeping();
 
 	/*State* pLobbyState = DBG_NEW LobbyState();
 	StateManager::GetInstance()->EnqueueStateTransition(pLobbyState, STATE_TRANSITION::POP_AND_PUSH);*/
@@ -80,8 +131,30 @@ void MainMenuGUI::OnButtonSettingsClick(BaseComponent* pSender, const RoutedEven
 {
 	UNREFERENCED_VARIABLE(pSender);
 	UNREFERENCED_VARIABLE(args);
+	m_ContextStack.push(m_pSettingsGroupBox);
+	ToggleView(m_pSettingsGroupBox);
+}
 
-	ToggleSettingsView();
+void MainMenuGUI::OnButtonKeybindingsClick(BaseComponent* pSender, const RoutedEventArgs& args)
+{
+	UNREFERENCED_VARIABLE(pSender);
+	UNREFERENCED_VARIABLE(args);
+	m_ContextStack.push(m_pKeybindingsGroupBox);
+	ToggleView(m_pKeybindingsGroupBox);
+}
+
+void MainMenuGUI::OnButtonApplyClick(BaseComponent* pSender, const RoutedEventArgs& args)
+{
+	UNREFERENCED_VARIABLE(pSender);
+	
+	InputActionSystem::ChangeKeyBinding("CAM_FORWARD", StringToKey(FrameworkElement::FindName<TextBox>("CAM_FORWARD")->GetText()));
+	InputActionSystem::ChangeKeyBinding("CAM_BACKWARD", StringToKey(FrameworkElement::FindName<TextBox>("CAM_BACKWARD")->GetText()));
+	InputActionSystem::ChangeKeyBinding("CAM_LEFT", StringToKey(FrameworkElement::FindName<TextBox>("CAM_LEFT")->GetText()));
+	InputActionSystem::ChangeKeyBinding("CAM_RIGHT", StringToKey(FrameworkElement::FindName<TextBox>("CAM_RIGHT")->GetText()));
+	InputActionSystem::ChangeKeyBinding("CAM_JUMP", StringToKey(FrameworkElement::FindName<TextBox>("CAM_JUMP")->GetText()));
+	InputActionSystem::ChangeKeyBinding("CAM_SPEED_MODIFIER", StringToKey(FrameworkElement::FindName<TextBox>("CAM_SPEED_MODIFIER")->GetText()));
+	
+	InputActionSystem::WriteToFile();
 }
 
 void MainMenuGUI::OnButtonBackClick(BaseComponent* pSender, const RoutedEventArgs& args)
@@ -89,28 +162,29 @@ void MainMenuGUI::OnButtonBackClick(BaseComponent* pSender, const RoutedEventArg
 	UNREFERENCED_VARIABLE(pSender);
 	UNREFERENCED_VARIABLE(args);
 
-	ToggleSettingsView();
+	if (m_ContextStack.top() == m_pStatesGroupBox)
+	{
+		m_pStatesGroupBox->SetVisibility(Visibility::Visibility_Collapsed);
+		m_pMainMenu->SetVisibility(Visibility::Visibility_Visible);
+	}
+	else
+	{
+		ToggleView(m_ContextStack.top());
+	}
+	m_ContextStack.pop();
 }
-
 
 void MainMenuGUI::OnButtonExitClick(BaseComponent* pSender, const RoutedEventArgs& args)
 {
 	UNREFERENCED_VARIABLE(pSender);
 	UNREFERENCED_VARIABLE(args);
 
-	using namespace LambdaEngine;
-	SetRenderStagesSleeping();
-
-	State* pStartingState = DBG_NEW BenchmarkState();
-	StateManager::GetInstance()->EnqueueStateTransition(pStartingState, STATE_TRANSITION::POP_AND_PUSH);
+	CommonApplication::Get()->Terminate();
 }
-
-
 
 void MainMenuGUI::OnRayTracingChecked(BaseComponent* pSender, const RoutedEventArgs& args)
 {
 	UNREFERENCED_VARIABLE(pSender);
-	LOG_WARNING("RT checked");
 
 	ToggleButton* pFE = (ToggleButton*)args.source;
 	m_RayTracingSleeping = pFE->GetIsChecked().GetValue();
@@ -119,7 +193,6 @@ void MainMenuGUI::OnRayTracingChecked(BaseComponent* pSender, const RoutedEventA
 void MainMenuGUI::OnMeshShadersChecked(BaseComponent* pSender, const RoutedEventArgs& args)
 {
 	UNREFERENCED_VARIABLE(pSender);
-	LOG_WARNING("MeshShaders checked");
 
 	ToggleButton* pFE = (ToggleButton*)args.source;
 	m_MeshShadersSleeping = pFE->GetIsChecked().GetValue();
@@ -128,13 +201,10 @@ void MainMenuGUI::OnMeshShadersChecked(BaseComponent* pSender, const RoutedEvent
 void MainMenuGUI::OnVolumeSliderChanged(BaseComponent* pSender, const RoutedPropertyChangedEventArgs<float>& args)
 {
 	AudioAPI::GetDevice()->SetMasterVolume(args.newValue);
-	LOG_MESSAGE("Changed volume! %f", AudioAPI::GetDevice()->GetMasterVolume());
 }
 
 void MainMenuGUI::SetRenderStagesSleeping()
 {
-	using namespace LambdaEngine;
-
 	RenderSystem::GetInstance().SetRenderStageSleeping("SKYBOX_PASS",				false);
 	RenderSystem::GetInstance().SetRenderStageSleeping("DEFERRED_GEOMETRY_PASS",	false);
 	RenderSystem::GetInstance().SetRenderStageSleeping("DIRL_SHADOWMAP",			false);
@@ -149,14 +219,16 @@ void MainMenuGUI::SetRenderStagesSleeping()
 
 }
 
-void MainMenuGUI::ToggleSettingsView()
+void MainMenuGUI::ToggleView(Noesis::FrameworkElement* element)
 {
-	if (m_pSettingsGroupBox->GetVisibility() == Visibility::Visibility_Hidden)
+	if (element->GetVisibility() == Visibility::Visibility_Hidden || 
+		element->GetVisibility() == Visibility::Visibility_Collapsed)
 	{
-		m_pSettingsGroupBox->SetVisibility(Visibility::Visibility_Visible);
+		element->SetVisibility(Visibility::Visibility_Visible);
 	}
 	else
 	{
-		m_pSettingsGroupBox->SetVisibility(Visibility::Visibility_Hidden);
+		element->SetVisibility(Visibility::Visibility_Hidden);
 	}
 }
+
