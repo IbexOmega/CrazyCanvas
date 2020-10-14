d080551d73c2e55486bd9f4e3e1b5810dcc34f32
Henry Bergström <henrybergstrom@protonmail.com>
2020-10-14 15:38:17 +0200

    added warning disable around rapidjason include again

--- CrazyCanvas/Source/World/LevelManager.cpp
+++ CrazyCanvas/Source/World/LevelManager.cpp
@@ -7,10 +7,12 @@
 
 #include "Log/Log.h"
 
+#pragma warning( push, 0 )
 #include <rapidjson/rapidjson.h>
 #include <rapidjson/document.h>
 #include <rapidjson/stringbuffer.h>
 #include <rapidjson/filereadstream.h>
+#pragma warning( pop )
 
 bool LevelManager::Init()
 {
