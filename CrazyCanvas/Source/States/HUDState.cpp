fc1787f385449910962f6aea08d3c7958a99c0dd
TimTheToad <tim.b.mellander@gmail.com>
2020-10-20 15:52:09 +0200

    cleaned up

--- CrazyCanvas/Source/States/HUDState.cpp
+++ CrazyCanvas/Source/States/HUDState.cpp
@@ -17,26 +17,18 @@ void HUDState::Init()
 {
 	using namespace LambdaEngine;
 
-	// Put unecessary renderstages to sleep in main menu
-
-	/*RenderSystem::GetInstance().SetRenderStageSleeping("SKYBOX_PASS", true);
-	RenderSystem::GetInstance().SetRenderStageSleeping("DEFERRED_GEOMETRY_PASS", true);
-	RenderSystem::GetInstance().SetRenderStageSleeping("DIRL_SHADOWMAP", true);
-	RenderSystem::GetInstance().SetRenderStageSleeping("FXAA", true);
-	RenderSystem::GetInstance().SetRenderStageSleeping("POINTL_SHADOW", true);
-	RenderSystem::GetInstance().SetRenderStageSleeping("SKYBOX_PASS", true);
-	RenderSystem::GetInstance().SetRenderStageSleeping("SHADING_PASS", true);*/
-
 	RenderSystem::GetInstance().SetRenderStageSleeping("RENDER_STAGE_NOESIS_GUI", false);
 
-	m_HUDGUI = *new HUDGUI("HUD.xaml");
-	m_View = Noesis::GUI::CreateView(m_HUDGUI);
+	m_HUDGUI	= *new HUDGUI("HUD.xaml");
+	m_View		= Noesis::GUI::CreateView(m_HUDGUI);
+
 	LambdaEngine::GUIApplication::SetView(m_View);
 }
 
 
 void HUDState::Tick(LambdaEngine::Timestamp delta)
 {
+	UNREFERENCED_VARIABLE(delta);
 	if (LambdaEngine::Input::GetMouseState().IsButtonPressed(LambdaEngine::EMouseButton::MOUSE_BUTTON_FORWARD))
 		m_HUDGUI->UpdateAmmo();
 }
