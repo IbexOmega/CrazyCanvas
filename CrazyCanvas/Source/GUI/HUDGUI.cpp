a63f9a3f9a3ba4ab4c0d6e15f4b413657681ef4b
TimTheToad <tim.b.mellander@gmail.com>
2020-10-21 15:35:05 +0200

    Cleanup before PR

--- CrazyCanvas/Source/GUI/HUDGUI.cpp
+++ CrazyCanvas/Source/GUI/HUDGUI.cpp
@@ -105,6 +105,7 @@ bool HUDGUI::UpdateScore()
 	m_GUIState.CurrentScore++;
 
 	scoreString = std::to_string(m_GUIState.CurrentScore) + "/" + std::to_string(MAX_SCORE);
+
 	FrameworkElement::FindName<TextBlock>("SCORE_DISPLAY")->SetText(scoreString.c_str());
 
 	if (m_GUIState.CurrentScore == MAX_SCORE)
@@ -120,10 +121,11 @@ bool HUDGUI::UpdateAmmo(const int32 currentAmmo, const int32 ammoCap)
 	//Returns false if Out Of Ammo
 	std::string ammoString;
 
-	m_GUIState.Ammo = currentAmmo;
+	m_GUIState.Ammo			= currentAmmo;
 	m_GUIState.AmmoCapacity = ammoCap;
 	
 	ammoString = std::to_string(m_GUIState.Ammo) + "/" + std::to_string(m_GUIState.AmmoCapacity);
+
 	FrameworkElement::FindName<TextBlock>("AMMUNITION_DISPLAY")->SetText(ammoString.c_str());
 
 	if (currentAmmo <= 0)
@@ -137,18 +139,18 @@ void HUDGUI::InitGUI()
 {
 	Rectangle* pHpRect = FrameworkElement::FindName<Rectangle>("HEALTH_RECT");
 
-	m_GUIState.DamageTaken = 0.0;
-	m_GUIState.LifeMaxHeight = pHpRect->GetHeight();
-	m_GUIState.Health = 100.0;
-	m_GUIState.Ammo = m_GUIState.AmmoCapacity;
-	m_GUIState.CurrentScore = 0;
+	m_GUIState.DamageTaken		= 0.0;
+	m_GUIState.LifeMaxHeight	= pHpRect->GetHeight();
+	m_GUIState.Health			= 100.0;
+	m_GUIState.Ammo				= m_GUIState.AmmoCapacity;
+	m_GUIState.CurrentScore		= 0;
 
 	pHpRect->SetHeight(0.0);
 
 	std::string scoreString;
 	std::string ammoString;
 
-	ammoString = std::to_string((int)m_GUIState.Ammo) + "/" + std::to_string((int)m_GUIState.AmmoCapacity);
+	ammoString	= std::to_string((int)m_GUIState.Ammo) + "/" + std::to_string((int)m_GUIState.AmmoCapacity);
 	scoreString = std::to_string(m_GUIState.CurrentScore) + "/" + std::to_string(MAX_SCORE);
 
 	FrameworkElement::FindName<TextBlock>("AMMUNITION_DISPLAY")->SetText(ammoString.c_str());
