eaf89fcc4c55c23bbeef4f98414c20f1fc50abba
Adrian <adde--97@hotmail.com>
2020-10-30 15:34:43 +0100

    Removed namespace

--- LambdaEngine/Source/Rendering/Core/API/PipelineContext.cpp
+++ LambdaEngine/Source/Rendering/Core/API/PipelineContext.cpp
@@ -7,125 +7,128 @@
 
 #include "Rendering/RenderAPI.h"
 
-bool LambdaEngine::PipelineContext::Init(const String& debugname)
+namespace LambdaEngine
 {
-	// Create pipeline layout
-	if (!m_Initilized)
+	bool PipelineContext::Init(const String& debugname)
 	{
-		PipelineLayoutDesc pipelineLayoutDesc = { };
-		pipelineLayoutDesc.DebugName = debugname + " Pipeline Layout";
-		pipelineLayoutDesc.DescriptorSetLayouts = m_DescriptorSetLayoutDescs;
-		pipelineLayoutDesc.ConstantRanges = m_ConstantRangeDescs;
+		// Create pipeline layout
+		if (!m_Initilized)
+		{
+			PipelineLayoutDesc pipelineLayoutDesc = { };
+			pipelineLayoutDesc.DebugName = debugname + " Pipeline Layout";
+			pipelineLayoutDesc.DescriptorSetLayouts = m_DescriptorSetLayoutDescs;
+			pipelineLayoutDesc.ConstantRanges = m_ConstantRangeDescs;
 
-		m_PipelineLayout = RenderAPI::GetDevice()->CreatePipelineLayout(&pipelineLayoutDesc);
+			m_PipelineLayout = RenderAPI::GetDevice()->CreatePipelineLayout(&pipelineLayoutDesc);
 
-		ManagedComputePipelineStateDesc pipelineDesc = { };
-		pipelineDesc.Shader.ShaderGUID = m_ComputeShaderGUID;
-		pipelineDesc.DebugName = debugname + " Pipeline State";
-		pipelineDesc.PipelineLayout = m_PipelineLayout;
+			ManagedComputePipelineStateDesc pipelineDesc = { };
+			pipelineDesc.Shader.ShaderGUID = m_ComputeShaderGUID;
+			pipelineDesc.DebugName = debugname + " Pipeline State";
+			pipelineDesc.PipelineLayout = m_PipelineLayout;
 
-		m_PipelineStateID = PipelineStateManager::CreateComputePipelineState(&pipelineDesc);
+			m_PipelineStateID = PipelineStateManager::CreateComputePipelineState(&pipelineDesc);
 		
 		
-		m_Initilized = m_PipelineStateID != NULL;
+			m_Initilized = m_PipelineStateID != NULL;
+		}
+		else
+		{
+			LOG_WARNING("[PipelineContext]: Can't Init, context has already been initilized");
+		}
+
+		return m_Initilized;
 	}
-	else
+
+	void PipelineContext::Update(Timestamp delta, uint32 modFrameIndex, uint32 backBufferIndex)
 	{
-		LOG_WARNING("[PipelineContext]: Can't Init, context has already been initilized");
+		m_DescriptorCache.HandleUnavailableDescriptors(modFrameIndex);
 	}
 
-	return m_Initilized;
-}
+	void PipelineContext::BindConstantRange(CommandList* pCommandList, void* pPushConstantData, uint32 pushConstantSize, uint32 pushConstantOffset)
+	{
+		pCommandList->SetConstantRange(m_PipelineLayout.Get(), FShaderStageFlag::SHADER_STAGE_FLAG_COMPUTE_SHADER, pPushConstantData, pushConstantSize, pushConstantOffset);
+	}
 
-void LambdaEngine::PipelineContext::Update(Timestamp delta, uint32 modFrameIndex, uint32 backBufferIndex)
-{
-	m_DescriptorCache.HandleUnavailableDescriptors(modFrameIndex);
-}
-
-void LambdaEngine::PipelineContext::BindConstantRange(CommandList* pCommandList, void* pPushConstantData, uint32 pushConstantSize, uint32 pushConstantOffset)
-{
-	pCommandList->SetConstantRange(m_PipelineLayout.Get(), FShaderStageFlag::SHADER_STAGE_FLAG_COMPUTE_SHADER, pPushConstantData, pushConstantSize, pushConstantOffset);
-}
-
-void LambdaEngine::PipelineContext::Bind(CommandList* pCommandList)
-{
-	pCommandList->BindComputePipeline(PipelineStateManager::GetPipelineState(m_PipelineStateID));
+	void PipelineContext::Bind(CommandList* pCommandList)
+	{
+		pCommandList->BindComputePipeline(PipelineStateManager::GetPipelineState(m_PipelineStateID));
 	
-	for (auto [key, descriptorSet] : m_DescriptorSets)
-	{
-		pCommandList->BindDescriptorSetCompute(descriptorSet.Get(), m_PipelineLayout.Get(), key);	
-	}
-}
-
-void LambdaEngine::PipelineContext::CreateDescriptorSetLayout(const TArray<DescriptorBindingDesc>& descriptorBindings)
-{
-	if (!m_Initilized)
-	{
-		DescriptorSetLayoutDesc descriptorSetLayoutDesc = {};
-		descriptorSetLayoutDesc.DescriptorBindings = descriptorBindings;
-
-		m_DescriptorSetLayoutDescs.PushBack(descriptorSetLayoutDesc);
-	}
-	else
-	{
-		LOG_WARNING("[PipelineContext]: Can't add DescriptorSetLayout, context has already been initilized");
-	}
-}
-
-void LambdaEngine::PipelineContext::CreateConstantRange(const ConstantRangeDesc& constantRangeDesc)
-{
-	if (!m_Initilized)
-	{
-		m_ConstantRangeDescs.PushBack(constantRangeDesc);
-	}
-	else
-	{
-		LOG_WARNING("[PipelineContext]: Can't add constantRange, context has already been initilized");
-	}
-}
-
-void LambdaEngine::PipelineContext::UpdateDescriptorSet(const String& debugname, uint32 setIndex, DescriptorHeap* pDescriptorHeap, const SDescriptorBufferUpdateDesc& descriptorUpdateDesc, bool shouldCopy)
-{
-	auto descriptorSet = m_DescriptorCache.GetDescriptorSet(debugname, m_PipelineLayout.Get(), setIndex, pDescriptorHeap, shouldCopy);
-	if (descriptorSet != nullptr)
-	{
-		descriptorSet->WriteBufferDescriptors(
-			descriptorUpdateDesc.ppBuffers,
-			descriptorUpdateDesc.pOffsets,
-			descriptorUpdateDesc.pSizes,
-			descriptorUpdateDesc.FirstBinding,
-			descriptorUpdateDesc.DescriptorCount,
-			descriptorUpdateDesc.DescriptorType
-		);
-
-		m_DescriptorSets[setIndex] = descriptorSet;
-	}
-	else
-	{
-		LOG_ERROR("[ParticleUpdater]: Failed to update DescriptorSet[%d]", 0);
-	}
-}
-
-void LambdaEngine::PipelineContext::UpdateDescriptorSet(const String& debugname, uint32 setIndex, DescriptorHeap* pDescriptorHeap, const SDescriptorTextureUpdateDesc& descriptorUpdateDesc, bool shouldCopy)
-{
-	auto descriptorSet = m_DescriptorCache.GetDescriptorSet(debugname, m_PipelineLayout.Get(), setIndex, pDescriptorHeap, shouldCopy);
-	if (descriptorSet != nullptr)
-	{
-		descriptorSet->WriteTextureDescriptors(
-			descriptorUpdateDesc.ppTextures,
-			descriptorUpdateDesc.ppSamplers,
-			descriptorUpdateDesc.TextureState,
-			descriptorUpdateDesc.FirstBinding,
-			descriptorUpdateDesc.DescriptorCount,
-			descriptorUpdateDesc.DescriptorType,
-			descriptorUpdateDesc.UniqueSamplers
-		);
-
-		m_DescriptorSets[setIndex] = descriptorSet;
-	}
-	else
-	{
-		LOG_ERROR("[ParticleUpdater]: Failed to update DescriptorSet[%d]", 0);
+		for (auto [key, descriptorSet] : m_DescriptorSets)
+		{
+			pCommandList->BindDescriptorSetCompute(descriptorSet.Get(), m_PipelineLayout.Get(), key);	
+		}
 	}
 
+	void PipelineContext::CreateDescriptorSetLayout(const TArray<DescriptorBindingDesc>& descriptorBindings)
+	{
+		if (!m_Initilized)
+		{
+			DescriptorSetLayoutDesc descriptorSetLayoutDesc = {};
+			descriptorSetLayoutDesc.DescriptorBindings = descriptorBindings;
+
+			m_DescriptorSetLayoutDescs.PushBack(descriptorSetLayoutDesc);
+		}
+		else
+		{
+			LOG_WARNING("[PipelineContext]: Can't add DescriptorSetLayout, context has already been initilized");
+		}
+	}
+
+	void PipelineContext::CreateConstantRange(const ConstantRangeDesc& constantRangeDesc)
+	{
+		if (!m_Initilized)
+		{
+			m_ConstantRangeDescs.PushBack(constantRangeDesc);
+		}
+		else
+		{
+			LOG_WARNING("[PipelineContext]: Can't add constantRange, context has already been initilized");
+		}
+	}
+
+	void PipelineContext::UpdateDescriptorSet(const String& debugname, uint32 setIndex, DescriptorHeap* pDescriptorHeap, const SDescriptorBufferUpdateDesc& descriptorUpdateDesc, bool shouldCopy)
+	{
+		auto descriptorSet = m_DescriptorCache.GetDescriptorSet(debugname, m_PipelineLayout.Get(), setIndex, pDescriptorHeap, shouldCopy);
+		if (descriptorSet != nullptr)
+		{
+			descriptorSet->WriteBufferDescriptors(
+				descriptorUpdateDesc.ppBuffers,
+				descriptorUpdateDesc.pOffsets,
+				descriptorUpdateDesc.pSizes,
+				descriptorUpdateDesc.FirstBinding,
+				descriptorUpdateDesc.DescriptorCount,
+				descriptorUpdateDesc.DescriptorType
+			);
+
+			m_DescriptorSets[setIndex] = descriptorSet;
+		}
+		else
+		{
+			LOG_ERROR("[ParticleUpdater]: Failed to update DescriptorSet[%d]", 0);
+		}
+	}
+
+	void PipelineContext::UpdateDescriptorSet(const String& debugname, uint32 setIndex, DescriptorHeap* pDescriptorHeap, const SDescriptorTextureUpdateDesc& descriptorUpdateDesc, bool shouldCopy)
+	{
+		auto descriptorSet = m_DescriptorCache.GetDescriptorSet(debugname, m_PipelineLayout.Get(), setIndex, pDescriptorHeap, shouldCopy);
+		if (descriptorSet != nullptr)
+		{
+			descriptorSet->WriteTextureDescriptors(
+				descriptorUpdateDesc.ppTextures,
+				descriptorUpdateDesc.ppSamplers,
+				descriptorUpdateDesc.TextureState,
+				descriptorUpdateDesc.FirstBinding,
+				descriptorUpdateDesc.DescriptorCount,
+				descriptorUpdateDesc.DescriptorType,
+				descriptorUpdateDesc.UniqueSamplers
+			);
+
+			m_DescriptorSets[setIndex] = descriptorSet;
+		}
+		else
+		{
+			LOG_ERROR("[ParticleUpdater]: Failed to update DescriptorSet[%d]", 0);
+		}
+
+	}
 }
