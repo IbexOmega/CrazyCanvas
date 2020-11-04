7c64de11f62bbb9d7b09114de788a99075c2671f
TimTheToad <tim.b.mellander@gmail.com>
2020-11-04 14:05:35 +0100

    minor fix

--- CrazyCanvas/Source/Match/MatchServer.cpp
+++ CrazyCanvas/Source/Match/MatchServer.cpp
@@ -145,11 +145,11 @@ void MatchServer::TickInternal(LambdaEngine::Timestamp deltaTime)
 
 							auto waterAmmo = weaponComp.WeaponTypeAmmo.find(EAmmoType::AMMO_TYPE_WATER);
 							if(waterAmmo != weaponComp.WeaponTypeAmmo.end())
-								ImGui::Text("Water Ammunition: %u/%u", waterAmmo->second.first, waterAmmo->second.first);
+								ImGui::Text("Water Ammunition: %u/%u", waterAmmo->second.first, waterAmmo->second.second);
 
 							auto paintAmmo = weaponComp.WeaponTypeAmmo.find(EAmmoType::AMMO_TYPE_PAINT);
 							if (paintAmmo != weaponComp.WeaponTypeAmmo.end())
-								ImGui::Text("Paint Ammunition: %u/%u", paintAmmo->second.first, paintAmmo->second.first);
+								ImGui::Text("Paint Ammunition: %u/%u", paintAmmo->second.first, paintAmmo->second.second);
 
 
 							if (ImGui::Button("Kill"))
