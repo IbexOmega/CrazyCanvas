813b836e4aca02c8e32bbb2bd92c2f5fc421b110
Christoffer Andersson <Christoffer1997.ca@gmail.com>
2020-11-04 13:23:58 +0100

    Renamed LobbyGUI to MultiplayerGUI

--- CrazyCanvas/Include/States/MultiplayerState.h
+++ CrazyCanvas/Include/States/MultiplayerState.h
@@ -1,17 +1,16 @@
 #pragma once
 #include "Game/State.h"
 
-#include "GUI/LobbyGUI.h"
+#include "GUI/MultiplayerGUI.h"
 
 #include "GUI/Core/GUIApplication.h"
 #include "NoesisPCH.h"
 
-
-class LobbyState : public LambdaEngine::State
+class MultiplayerState : public LambdaEngine::State
 {
 public:
-	LobbyState() = default;
-	~LobbyState();
+	MultiplayerState() = default;
+	~MultiplayerState();
 
 	void Init() override final;
 
@@ -22,6 +21,6 @@ public:
 	void FixedTick(LambdaEngine::Timestamp delta) override;
 
 private:
-	Noesis::Ptr<LobbyGUI> m_LobbyGUI;
+	Noesis::Ptr<MultiplayerGUI> m_LobbyGUI;
 	Noesis::Ptr<Noesis::IView> m_View;
 };
