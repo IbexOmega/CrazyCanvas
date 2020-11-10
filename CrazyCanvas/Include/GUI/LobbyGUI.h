fa5da4966cc023a065a91c9c8329939816fc6489
Christoffer Andersson <Christoffer1997.ca@gmail.com>
2020-11-10 16:48:03 +0100

    Update CrazyCanvas/Include/GUI/LobbyGUI.h
    Co-authored-by: Daniel Cheh <vonsenmc@gmail.com>

--- CrazyCanvas/Include/GUI/LobbyGUI.h
+++ CrazyCanvas/Include/GUI/LobbyGUI.h
@@ -50,7 +50,7 @@ public:
 private:
 	// Helpers
 	void AddColumnDefinitionStar(Noesis::ColumnDefinitionCollection* columnCollection, float width);
-	void AddLabelWithStyle(const LambdaEngine::String& name, Noesis::Panel* parent, const LambdaEngine::String& styleKey, const LambdaEngine::String& content);
+	void AddLabelWithStyle(const LambdaEngine::String& name, Noesis::Panel* pParent, const LambdaEngine::String& styleKey, const LambdaEngine::String& content);
 	void RegisterName(const LambdaEngine::String& name, Noesis::BaseComponent* comp);
 	void CreateHostIcon(Noesis::Panel* parent);
 	Noesis::Grid* GetPlayerGrid(const Player& player);
