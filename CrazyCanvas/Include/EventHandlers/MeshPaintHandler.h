636d5c9ee722ea2f9976067eed5ba846282dde50
Jonathan Åleskog <jonathan9609082000@gmail.com>
2020-11-18 16:28:21 +0100

    Removed many things, seems to work

--- CrazyCanvas/Include/EventHandlers/MeshPaintHandler.h
+++ CrazyCanvas/Include/EventHandlers/MeshPaintHandler.h
@@ -38,22 +38,6 @@ public:
 	void Init();
 
 private:
-	struct PaintPoint
-	{
-		glm::vec3 Position;
-		glm::vec3 Direction;
-		LambdaEngine::EPaintMode	PaintMode	= LambdaEngine::EPaintMode::NONE;
-		LambdaEngine::ERemoteMode	RemoteMode	= LambdaEngine::ERemoteMode::UNDEFINED;
-		LambdaEngine::ETeam			Team		= LambdaEngine::ETeam::NONE;
-		uint32 Angle;
-	};
-
-private:
 	bool OnProjectileHit(const ProjectileHitEvent& projectileHitEvent);
 	bool OnPacketProjectileHitReceived(const PacketReceivedEvent<PacketProjectileHit>& event);
-
-	bool IsPaintPointEqual(PaintPoint& a, PaintPoint& b);
-
-private:
-	std::queue<PaintPoint> m_PaintPointsOnClient;
 };
