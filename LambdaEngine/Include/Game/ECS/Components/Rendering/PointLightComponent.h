85f447a46b4babca18559fb6957c9a6e579f6849
Adrian <adde--97@hotmail.com>
2020-12-02 09:56:42 +0100

    Fixed ambient in raytracingh and added showers

--- LambdaEngine/Include/Game/ECS/Components/Rendering/PointLightComponent.h
+++ LambdaEngine/Include/Game/ECS/Components/Rendering/PointLightComponent.h
@@ -8,7 +8,7 @@ namespace LambdaEngine
 	{
 		DECL_COMPONENT_WITH_DIRTY_FLAG(PointLightComponent);
 		glm::vec4	ColorIntensity	= glm::vec4(1.0);
-		float		NearPlane		= 0.1f;
+		float		NearPlane		= 0.01f;
 		float		FarPlane		= 25.0f;
 	};
 }
