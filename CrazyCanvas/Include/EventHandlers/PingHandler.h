48b7e8e5d81fa9a201db0aa26734d8d6ee7adc26
TheoBerlin <theo.holmqvist@gmail.com>
2020-12-08 10:11:12 +0100

    Networked position pings

--- CrazyCanvas/Include/EventHandlers/PingHandler.h
+++ CrazyCanvas/Include/EventHandlers/PingHandler.h
@@ -2,6 +2,8 @@
 
 #include "Containers/IDVector.h"
 #include "ECS/EntitySubscriber.h"
+#include "Events/PacketEvents.h"
+#include "Multiplayer/Packet/PacketPositionPing.h"
 
 #define MAX_PING_DISTANCE 100.0f
 #define PING_DURATION 8.0f // Seconds until a ping is removed (unless it is replaced with a new ping)
@@ -21,7 +23,10 @@ public:
 	void Init();
 
 private:
+	void Ping(LambdaEngine::Entity pingingPlayer, const glm::vec3& pingPosition, const glm::vec3& cameraPosition, uint8 teamIndex);
+
 	bool OnKeyPress(const LambdaEngine::KeyPressedEvent& keyPressEvent);
+	bool OnPacketPositionPingReceived(const PacketReceivedEvent<PacketPositionPing>& pingEvent);
 
 private:
 	// Map player entities (not camera entities) to their current ping entities
