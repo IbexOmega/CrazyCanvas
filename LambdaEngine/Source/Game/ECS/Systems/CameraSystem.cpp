27c02b7b8ec88123a9efd92cda13410c29025d1f
Alexander Dahlin <alexander.dahlins@gmail.com>
2020-12-11 16:27:28 +0100

    Update CameraSystem.cpp

--- LambdaEngine/Source/Game/ECS/Systems/CameraSystem.cpp
+++ LambdaEngine/Source/Game/ECS/Systems/CameraSystem.cpp
@@ -134,7 +134,7 @@ namespace LambdaEngine
 							glm::cos(cameraComp.ScreenShakeAngle) * cameraComp.ScreenShakeTime,
 							glm::sin(cameraComp.ScreenShakeAngle) * cameraComp.ScreenShakeTime,
 							0.0f) * cameraComp.ScreenShakeAmplitude);
-				cameraComp.ScreenShakeTime -= deltaTime.AsSeconds();
+				cameraComp.ScreenShakeTime -= float32(deltaTime.AsSeconds());
 			}
 		}
 	}
